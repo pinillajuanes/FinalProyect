4578e45c3cee121d9bf5199a87645f8fe6398556
unknown <pinillaguilerajuanes@gmail.com>
2022-06-01 00:03:31 -0500

    first commit

---
+++ Clase3Corte/Clase3Corte/Data/Libro.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Clase3Corte.Data
+{
+    public class Comida
+    {
+        [Key]
+        public int IdComida { get; set; }
+        public string? Nombre { get; set; }
+        public string? Categoria { get; set; }
+        public string? PaisOrigen { get; set; }
+        public int Costo { get; set; }
+        public string? Descripcion { get; set; }
+    }
+}
---
+++ Clase3Corte/Clase3Corte/Data/Videojuegos.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Clase3Corte.Data
+{
+    public class Videojuego
+    {
+        [Key]
+        public int IdJuego { get; set; }
+        public string? Titulo { get; set; }
+        public string? Consola { get; set; }
+        public string? Desarrollador { get; set; }
+        public string? Categoria { get; set; }
+        public string? Descripcion { get; set; }
+    }
+}
